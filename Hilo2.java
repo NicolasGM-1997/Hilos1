6a501b6248c43493acad8b0a98dae454ea5346d1
Nicolas Gomez <nicolas.gomezm@uniagustiniana.edu.co>
2023-08-27 10:41:12 -0500

    Commit de hilos

---
+++ Hilo2.java
@@ -0,0 +1,18 @@
+package hilos;
+
+public class Hilo2 implements Runnable {
+    
+    @Override public void run(){
+        int y = 1, x = 0;
+        try{
+            while(y>0) {
+                System.out.println("Numero "+x);
+                x++;
+                Thread.sleep(1200);
+            }
+        }catch(Exception ex){
+            System.out.println(""+ex);
+        }
+    }
+    
+}
